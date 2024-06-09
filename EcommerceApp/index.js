c881edc769ae8f4ffdee454aea8f8e4d99b5adc8
Devendra Shukla <devshukla0123@gmail.com>
2024-06-09 17:25:37 +0530

    commit backend code

---
+++ EcommerceApp/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const port = 3000
+
+app.get('/', (req, resp) => {
+    resp.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`EcommerceApp listening on port http://localhost:${port}`)
+})
