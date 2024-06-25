da575075ea044b5d419c848614c7750340ec506f
Devendra Shukla <devshukla0123@gmail.com>
2024-06-25 18:35:50 +0530

    commit login code

--- router/auth.js
+++ router/auth.js
@@ -34,8 +34,8 @@ router.post('/register', [
         //way-1 for try and catch
         let user = await User.create({
             name: req.body.name,
-            email: secPassword,
-            password: req.body.password
+            password: secPassword,
+            email: req.body.email
         });
         let data = {
            user:{
@@ -65,8 +65,8 @@ router.post('/register', [
     /*
     User.create({
         name:req.body.name,
-        email:secPassword,
-        password:req.body.password
+        email:req.body.email,
+        password:secPassword
     })
     .then(user => resp.status(200).json({
         _id:user._id,
@@ -79,6 +79,47 @@ router.post('/register', [
         return resp.status(401).json({error:'Please enter a unique email address',message:err.message})
     });
     */
+});
+
+//url: /api/auth/login
+router.post('/login', [
+    body("email", 'Enter a valid email').isEmail(),
+    body("password", 'Password can not be blank').exists(),
+], async (req, resp) => { 
+    //return errors when any validation true
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return resp.status(400).json({ error: errors.array() });
+    }
+    
+    try {
+        const {email,password} = req.body;
+        console.log(email,password);
+        let selUser = await User.findOne({ email });
+        if (!selUser) {
+            return resp.status(400).json({ error: "Email address is not valid." });
+        }
+        const compairPassword = await bcrypt.compare(password,selUser.password);
+        if(!compairPassword) {
+            return resp.status(400).json({ error: "Password not valid. Please enter a correct password." });
+        }
+        let data = {
+            user:{
+             id: selUser._id
+            }
+        }
+        const authtoken = JWT.sign(data,jwtkey);
+        resp.send({authtoken});
+
+        //way-1 for try and catch
+        // let user = await User.create({
+        //     name: req.body.name,
+        //     email: secPassword,
+        //     password: req.body.password
+        // });
+    } catch (error) {
+        return resp.status(401).json({ error: 'Internal server error', message: error.message })
+    }
 });
 
 module.exports = router;
