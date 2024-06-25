14e0d6ba9a06468be3d64b6fb241973a9d2cbdf1
Devendra Shukla <devshukla0123@gmail.com>
2024-06-25 12:42:37 +0530

    check body params with validation and message also send

--- router/auth.js
+++ router/auth.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const User = require("../Models/User");
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 router.get('/', (req, resp) => { //url: /api/auth/
     resp.send("iNotbook api");
 });
 
-router.post('/register',  async (req, resp) => { //url: /api/auth/register
-    let data = new User(req.body);
-    let result = await data.save();
-    resp.send(result);
+router.post('/register', [
+    body("name",'Enter a valid name').isLength({ min: 5 }),
+    body("email",'Enter a valid email').isEmail(),
+    body("password",'Enter a valid password').isLength({ min: 5 }),
+], async (req, resp) => { //url: /api/auth/register
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return resp.status(400).json({error:errors.array()});
+    }
+
+    // let data = new User(req.body);
+    // let result = await data.save();
+    // resp.send(result);
 });
 
 module.exports = router;
