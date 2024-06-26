b97747c45aff93ad5f15cad5b6a181304dd68a7c
Devendra Shukla <devshukla0123@gmail.com>
2024-06-26 13:15:05 +0530

    code for delete note

--- router/notes.js
+++ router/notes.js
@@ -62,7 +62,27 @@ router.put('/updatenote/:id', fetchuser, async (req, resp) => {
             return resp.status(401).json({error:'You not able to update this note.'});
         }
         result = await Notes.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true});
-        resp.status(200).json(result);
+        resp.status(200).json({"Success":"Note has been updated.",note:result});
+    } catch (error) {
+        return resp.status(500).json({ error: 'Internal server error', message: error.message })
+    }
+});
+
+//Route to delete notes
+//Url: /api/notes/deletenote
+router.delete('/deletenote/:id', fetchuser, async (req, resp) => {
+    try {
+        //check note id is valid or not
+        const note = await Notes.findById(req.params.id); 
+        if(!note) {
+            return resp.status(404).json({error:'Note id not found!'});
+        }
+        // check updated note is login user user note or not
+        if(note.user.toString() != req.user.id) {
+            return resp.status(401).json({error:'You not able to update this note.'});
+        }
+        result = await Notes.findByIdAndDelete(req.params.id);
+        resp.status(200).json({"Success":"Note has been deleted.",note:result});
     } catch (error) {
         return resp.status(500).json({ error: 'Internal server error', message: error.message })
     }
