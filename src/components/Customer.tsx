a5df178dcdae7ac62f1d4c7d63613c071738178e
Devendra Shukla <devshukla0123@gmail.com>
2024-07-14 13:01:05 +0530

    complete code

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 //create interface for getting props
 interface IPROPS {
-    name:String,
-    prabhu:String,
-    age?:Number//? defines that its a optional parameter.
+    name: String;
+    prabhu: String;
+    age?: Number;//? defines that its a optional parameter.
 }
-const Customer: React.FC<IPROPS> = ({name,prabhu}) => {
+const Customer: React.FC<IPROPS> = ({ name, prabhu }) => {
     return (
         <>
             <div className="container">
                 <h2>Its my customer functional component in react typescript</h2>
                 <div>God: {prabhu}</div>
                 <div>Name: {name}</div>
-                
+
             </div>
         </>
     )
