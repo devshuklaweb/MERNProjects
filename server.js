edd82d81dba9ad13f647226b32b1828710bfef64
Devendra Shukla <devshukla0123@gmail.com>
2024-11-17 14:29:57 +0530

    Securing Your Private Data with Dotenv in our Backend App | MERN

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();//ye sbse top pe likhna hoga use karne ke liye.
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 //for accepting json inputs
 app.use(express.json());
--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const URI = "mongodb://localhost:27017/website_mern";
+const URI = process.env.MONGOOSE_URI;
 
 const connectDB = async () => {
 
