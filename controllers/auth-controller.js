83b6ad025b8bf371e51050c9f34a703946588f96
Devendra Shukla <devshukla0123@gmail.com>
2024-11-17 15:22:00 +0530

    Storing Registered User Data in an Online Database with Express and Mongoose

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,25 +1,34 @@
+const User = require('../models/user-model')
 
 const home = async (req, resp) => {
-
-    try {
-
-        resp.status(200).send("auth controller using auth-router");
-
-    } catch(err) {
-        console.log(err);
-    }
-
+  try {
+    resp.status(200).send('auth controller using auth-router')
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-const register = async (req,resp) => {
-
-    try {
-        console.log(req.body);
-        //resp.status(200).send("register-auth controller using auth-router");
-        resp.status(200).send({inputs: req.body});
-    } catch(error) {
-        resp.status(400).send("Error found. "+error);
+const register = async (req, resp) => {
+  try {
+    const { username, email, phone, password } = req.body
+    const userExist = await User.findOne({ email: email })
+    if (userExist) {
+      resp.status(400).json({ message: 'User email already exist.' })
     }
+    const userCreated = await User.create({
+      username: username,
+      email: email,
+      phone: phone,
+      password: password
+    })
+    resp.status(200).send({ message: userCreated })
+
+    //console.log(req.body);
+    //resp.status(200).send("register-auth controller using auth-router");
+    //resp.status(200).send({ inputs: req.body })
+  } catch (error) {
+    resp.status(500).send('Internal server error and error is: ' + error)
+  }
 }
 
-module.exports = {home,register};
+module.exports = { home, register }
