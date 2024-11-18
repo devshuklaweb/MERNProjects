41497ee8db1ae20598402adc68ddb52bd50863b1
Devendra Shukla <devshukla0123@gmail.com>
2024-11-18 12:17:02 +0530

    Secure User Password using Bcrypt.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user-model')
+//const bcrypt = require('bcrypt')
 
 const home = async (req, resp) => {
   try {
@@ -13,8 +14,9 @@ const register = async (req, resp) => {
     const { username, email, phone, password } = req.body
     const userExist = await User.findOne({ email: email })
     if (userExist) {
-      resp.status(400).json({ message: 'User email already exist.' })
+      return resp.status(400).json({ message: 'User email already exist.' })
     }
+
     const userCreated = await User.create({
       username: username,
       email: email,
@@ -27,7 +29,8 @@ const register = async (req, resp) => {
     //resp.status(200).send("register-auth controller using auth-router");
     //resp.status(200).send({ inputs: req.body })
   } catch (error) {
-    resp.status(500).send('Internal server error and error is: ' + error)
+    console.log(error)
+    resp.status(200).send({ message: 'Internal server error and error' })
   }
 }
 
--- models/user-model.js
+++ models/user-model.js
@@ -1,29 +1,53 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type:String,
-        require:true
+      type: String,
+      require: true
     },
     email: {
-        type:String,
-        require:true
+      type: String,
+      require: true
     },
     password: {
-        type:String,
-        require:true
+      type: String,
+      require: true
     },
     phone: {
-        type:String,
-        require:true
+      type: String,
+      require: true
     },
     isAdmin: {
-        type:Boolean,
-        default:false
+      type: Boolean,
+      default: false
     }
-});
+  },
+  {
+    timestamps: { createdAt: 'created_on', updatedAt: 'updated_on' }
+  }
+)
+
+// Pre-save middleware to secure the password using bcrypt
+userSchema.pre('save', async function (next) {
+  const user = this
+
+  if (!user.isModified('password')) {
+    next() //when password not modified
+  }
+  try {
+    // for encrypt password
+    const saltRounds = await bcrypt.genSalt(10)
+    const has_password = await bcrypt.hash(user.password, saltRounds)
+    user.password = has_password
+  } catch (error) {
+    next(error)
+  }
+  next()
+})
 
 //define the modal name or collection name
-const User = mongoose.model("User",userSchema); //first collection name in singular form and second is schema
+const User = mongoose.model('User', userSchema) //first collection name in singular form and second is schema
 
-module.exports = User;
+module.exports = User
