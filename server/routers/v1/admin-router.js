dce25c359fc51fa09cfbe34a05c06135a5f996d2
Devendra Shukla <devshukla0123@gmail.com>
2024-12-07 13:16:10 +0530

    updateUserById route commit

--- server/routers/v1/admin-router.js
+++ server/routers/v1/admin-router.js
@@ -8,6 +8,7 @@ const adminMiddleware = require("../../middlewares/adminMiddleware");
 router.route("/users").get(authMiddleware, adminMiddleware, adminController.getAllUsers);
 router.route("/users/delete/:id").delete(authMiddleware, adminMiddleware, adminController.deleteUserById);
 router.route("/users/:id").get(authMiddleware, adminMiddleware, adminController.getUserById);
+router.route("/users/update/:id").patch(authMiddleware, adminMiddleware, adminController.updateUserById);
 
 router.route("/contacts").get(authMiddleware, adminController.getAllContacts);
 router.route("/contacts/delete/:id").delete(authMiddleware, adminMiddleware, adminController.deleteContactById);
