619d5376388f54146b55ea7f56bc1e7f5c0476f7
Devendra Shukla <devshukla0123@gmail.com>
2025-01-02 15:14:01 +0530

    un required files removed

--- server/middlewares/error-middleware.js
+++
@@ -1,9 +0,0 @@
-const errorMiddleware = (err, res, resp, next) => {
-  const status = err.status || 500
-  const message = err.message || 'Backend Error'
-  const extraDetails = err.extraDetails || 'Backend Error'
-
-  return resp.status(status).json({ message, extraDetails })
-}
-
-module.exports = errorMiddleware
