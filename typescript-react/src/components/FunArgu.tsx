3510092849f9292e0a6144e93c129067525205b0
Devendra Shukla <devshukla0123@gmail.com>
2025-01-03 12:43:07 +0530

    Function With Parameters In React Typescript

--- typescript-react/src/components/FunArgu.tsx
+++ typescript-react/src/components/FunArgu.tsx
@@ -6,14 +6,8 @@ const FunArgu: React.FC = () => {
     const [state, setState] = useState < IState > ({
         message: 'Welcome Shri Ram'
     });
-    const handleLike = (str:string):void => {
+    const handleGreet = (str:string):void => {
         setState({message:str+" Shri Ram"})
-    }
-    const handleComment = (str: string): void => {
-        setState({ message: str + " Shri Ram" })
-    }
-    const handleSubscribe = (str: string): void => {
-        setState({ message: str + " Shri Ram" })
     }
 
     return (
@@ -23,9 +17,9 @@ const FunArgu: React.FC = () => {
                     <h5 className="card-title">Function With Parameters In React Typescript</h5>
                     <div><strong>Trigger Button Message:</strong> {state.message}</div>
                     <div className='container'>
-                        <button className="btn btn-sm btn-primary m-2" onClick={() => handleLike('Like')}>Like</button>
-                        <button className="btn btn-sm btn-success m-2" onClick={() => handleComment('Comment')}>Comment</button>
-                        <button className="btn btn-sm btn-info m-2" onClick={() => handleSubscribe('Subscribe')}>Subscribe</button>
+                        <button className="btn btn-sm btn-primary m-2" onClick={() => handleGreet('Like')}>Like</button>
+                        <button className="btn btn-sm btn-success m-2" onClick={() => handleGreet('Comment')}>Comment</button>
+                        <button className="btn btn-sm btn-info m-2" onClick={() => handleGreet('Subscribe')}>Subscribe</button>
                     </div>
                 </div>
             </div>
