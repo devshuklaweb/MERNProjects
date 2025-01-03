d0de70b678ca70c4d285bcd2efbec4576e2495bd
Devendra Shukla <devshukla0123@gmail.com>
2025-01-03 12:05:19 +0530

    Handling State with react typescript

--- typescript-react/src/components/Classprops.tsx
+++ typescript-react/src/components/Classprops.tsx
@@ -4,21 +4,39 @@ interface IPROPS {
     address: String;
     age?: number; //? show its an optional    
 }
-export default class Classprops extends React.Component<IPROPS> {
+interface IState {
+    name: String;
+    email: String;
+}
+export default class Classprops extends React.Component<IPROPS,IState> {
 
     constructor(props:IPROPS) {
         super(props);
-
+        this.state = {
+            name: 'Deepak',
+            email: 'devendra@gmail.com'
+        }
     }
     render() {
         let { name, address, age } = this.props;
         return (
-            <div className='card'>
-                <h1>Class Components Props In React Typescript</h1>
-                <div>Name: {name}</div>
-                <div>Address: {address}</div>
-                <div>Age: {age ?? 0}</div>
-            </div>
+            <>
+                <div className='card m-2'>
+                    <div className="card-body">
+                        <h5 className="card-title">Class Components Props In React Typescript</h5>
+                        <div>Name: {name}</div>
+                        <div>Address: {address}</div>
+                        <div>Age: {age ?? 0}</div>
+                    </div>
+                </div>
+                <div className='card m-2'>
+                    <div className="card-body">
+                        <h5 className="card-title">Handling State Class Component with react typescript</h5>
+                        <div>Name: {this.state.name}</div>
+                        <div>Email: {this.state.email}</div>
+                    </div>
+                </div>
+            </>
         );
     }
 }
--- typescript-react/src/components/Customer.tsx
+++ typescript-react/src/components/Customer.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 interface IPROPS {
     name: String;
     address: String;
     age ?: number; //? show its an optional    
 }
+interface IState {
+    name: String;
+    email: String;
+}
 const Customer: React.FC<IPROPS> = ({ name, address, age }) => {
-    //{name,address,age} = Object destructor kiya gya hai
+    const [state, setState] = useState<IState>({
+        name: 'Shri Ram',
+        email: 'dev@gmail.com',
+    })
     return (
-        <div className='card'>
-            <h1>Functional Components Props In React Typescript</h1>
-            <div>Name: {name}</div>
-            <div>Address: {address}</div>
-            {age && <div>Age: {age ?? 0}</div>}
-        </div>
+        <>
+            <div className='card m-2'>
+                <div className="card-body">
+                    <h5 className="card-title">Class Components Props In React Typescript</h5>
+                    <div>Name: {name}</div>
+                    <div>Address: {address}</div>
+                    {age && <div>Age: {age ?? 0}</div>}
+                </div>
+            </div>
+            <div className='card m-2'>
+                <div className="card-body">
+                    <h5 className="card-title">Handling State Class Component with react typescript</h5>
+                    <div>Name: {state.name}</div>
+                    <div>Email: {state.email}</div>
+                </div>
+            </div>
+        </>
     );
 };
 
